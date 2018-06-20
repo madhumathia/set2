0addd4e2214bd7e61a0605a9dea9e542444c0d39
madhumathia <34538824+madhumathia@users.noreply.github.com>
2018-06-20 06:21:55 +0530

    Rename power.c to powernum.c

---
+++ powernum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+    int num,power,p;
+    scanf("%d",&num);
+    scanf("%d",&p);
+    power=pow(num,p);
+    printf("The result is : %d",power);
+}
